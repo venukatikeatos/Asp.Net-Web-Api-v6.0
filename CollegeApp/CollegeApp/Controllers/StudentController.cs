2efc6509b62f8bb29fa2882f5c58863fc534f04c
venukatikeatos <venukatike@atos.net>
2024-01-01 18:35:32 +0530

    Simple Web Api v6.0

---
+++ CollegeApp/CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.Students;
+        }
+
+        [HttpGet("{id:int}")]
+        public Student GetStudentById(int id)
+        {
+            return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ CollegeApp/CollegeApp/Models/CollegeRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>{
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Test",
+                    Email = "Test@gmail.com",
+                    Address ="Hyd,India"
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Test1",
+                    Email = "Test1@gmail.com",
+                    Address ="Hyd,India"
+                },
+                new Student
+                {
+                    Id = 3,
+                    StudentName = "Test2",
+                    Email = "Test2@gmail.com",
+                    Address ="Hyd,India"
+                }
+                };
+    }
+}
